de3c98d5e4568f35c691e8ecf9ed74f8b6ba32c6
Ewerton I. Barreto <iohrannewerton@gmail.com>
2022-02-23 00:32:42 -0300

    Inserção de nova activity e ajustes

--- app/src/main/java/br/com/fiap/codex/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/codex/LoginActivity.kt
@@ -1,13 +1,13 @@
 package br.com.fiap.codex
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//Primeiro método que roda, quando o app é aberto
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) //redesenha a tela e mantém os dados do usuário
-        setContentView(R.layout.activity_main) //vinculação entre o kotlyn e o layout
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
     }
 }
 
---
+++ app/src/main/java/br/com/fiap/codex/StartActivity.kt
@@ -0,0 +1,24 @@
+package br.com.fiap.codex
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+//Primeiro método que roda, quando o app é aberto
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) //redesenha a tela e mantém os dados do usuário
+        setContentView(R.layout.activity_start) //vinculação entre o kotlyn e o layout
+
+        //Chamada para outra activity interna
+        val btnAbrirAdm = findViewById<Button>(R.id.buttonAdm)
+        val btnAbrirColab = findViewById<Button>(R.id.buttonColab)
+
+        btnAbrirAdm.setOnClickListener {
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
